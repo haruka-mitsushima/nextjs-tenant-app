ea242b558a2958b66f54ab3a953a23fda5fb3020
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2022-11-15 16:53:29 +0900

    rect-hook-formを使って商品登録フォームを作り直しました。

--- src/pages/items/create.tsx
+++ src/pages/items/create.tsx
@@ -1,50 +1,49 @@
 import Head from 'next/head';
-import { useState } from 'react';
+import {useForm, SubmitHandler,} from 'react-hook-form';
 
 type Inputs = {name: string, description: string, price: number, imageURL: string}
 
 export default function CreatePage(){
-    const [name, setName] = useState('')
-    const [description, setDescription] = useState('')
-    const [price, setPrice] = useState('')
-    const [imageURL, setImageURL] = useState('')
-
-    const send = async () =>{
+    const {
+        register,
+        handleSubmit,
+        reset,
+    } = useForm<Inputs>();
+    const send: SubmitHandler<Inputs> = async (data) =>{
         await fetch('http://localhost:3000/api/items', {
             method: 'POST',
             headers: {'Content-Type': 'application/json',},
-            body: JSON.stringify({name: name, description: description, price: price, imageURL: imageURL})
-        })
+            body: JSON.stringify(data)
+        }).then(()=>{reset();})
     }
-    
     return (
         <>
         <Head>
             <title>商品登録ページ</title>
         </Head>
         <h1>商品登録フォーム</h1>
-        <form method="psot" >
+        <form method="psot" onSubmit={handleSubmit(send)} id="form" >
             <label htmlFor="name">
                 商品名:
-                <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" id="name" {...register("name")} />
             </label>
             <br />
             <label htmlFor="description">
                 商品の説明:
-                <input type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <input type="text" id="description" {...register("description")} />
             </label>
             <br />
             <label htmlFor="price">
                 商品の価格:
-                <input type="text" id="price" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+                <input type="text" id="price" {...register("price")} />
             </label>
             <br />
             <label htmlFor="imageURL">
                 商品画像のURL:
-                <input type="url" id="imageURL" name="imageURL" value={imageURL} onChange={(e) => setImageURL(e.target.value)} />
+                <input type="url" id="imageURL" {...register("imageURL")} />
             </label>
             <br />
-            <input type="submit" onClick={send}/>
+            <input type="submit" />
         </form>
         </>
     )
