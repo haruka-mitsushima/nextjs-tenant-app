6a0fd21d1d840365ab4e691c851a0476f8fcacab
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-20 14:13:42 +0900

    初回

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,46 +1,69 @@
-import useSWR, {useSWRConfig} from 'swr';
+import useSWR, { useSWRConfig } from 'swr';
 
-const fetcher = (resource: string, init: any) => fetch(resource, init).then((res) => res.json());
-type Item= {id: number, name: string, description: string, deleted: boolean}
-
-export default function ItemList () {
-    const {mutate} = useSWRConfig()
-    const {data, error} = useSWR('/api/items', fetcher)
-    if(error) return <div>failed to load</div>
-    if(!data) return <div>loading...</div>
-        return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>商品名</th>
-                    <th>説明</th>
-                    <th>操作</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.filter((item: Item)=>{if(item.deleted === false){return item}}).map((item: Item) => {
-                    return (
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.description}</td>
-                            <td><button onClick ={async ()=>{
-                                    const info = {deleted: true}
-                                    await fetch(`http://localhost:3000/api/items/${item.id}`, {
-                                        method: 'PATCH',
-                                        headers: {'Content-Type': 'application/json',},
-                                        body: JSON.stringify(info)
-                                    })
-                                    mutate(`/api/items`)
-                            }}>削除</button></td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-    );
+const fetcher = (resource: string, init: any) =>
+  fetch(resource, init).then((res) => res.json());
+type Item = {
+  id: number;
+  name: string;
+  description: string;
+  deleted: boolean;
 };
+
+export default function ItemList() {
+  const { mutate } = useSWRConfig();
+  const { data, error } = useSWR('/api/items', fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>商品名</th>
+          <th>説明</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data
+          .filter((item: Item) => {
+            if (item.deleted === false) {
+              return item;
+            }
+          })
+          .map((item: Item) => {
+            return (
+              <tr key="tr1">
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button
+                    onClick={async () => {
+                      const info = { deleted: true };
+                      await fetch(
+                        `http://localhost:3000/api/items/${item.id}`,
+                        {
+                          method: 'PATCH',
+                          headers: {
+                            'Content-Type': 'application/json',
+                          },
+                          body: JSON.stringify(info),
+                        }
+                      );
+                      mutate(`/api/items`);
+                    }}
+                  >
+                    削除
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  );
+}
 
 // export async function getItemData(id: number){
 //     const {data} = useSWR('/api/items', fetcher)
---
+++ src/pages/api/user.ts
@@ -0,0 +1,26 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from 'lib/session'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export type User = {
+    isLoggedIn: boolean
+    login: string
+    avatarUrl: string
+  }
+
+async function userRoute(req: NextApiRequest, res: NextApiResponse<User>){
+    if(req.session.user){
+        res.json({
+            ...req.session.user,
+            isLoggedIn: true,
+        })
+    } else {
+        res.json({
+            isLoggedIn: false,
+            login: '',
+            avatarUrl: '',
+        })
+    }
+}
+
+export default withIronSessionApiRoute (userRoute, sessionOptions)
